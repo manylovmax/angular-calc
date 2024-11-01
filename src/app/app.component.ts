60d534242e5759fe025f1ed1a1db924c46f39221
manylovmax <maximalist000@gmail.com>
2024-11-01 19:50:48 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalcComponent } from '../calc/calc.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalcComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-test';
-}
+export class AppComponent {}
